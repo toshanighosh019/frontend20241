4d7345aaedffdfdd773af063acf845df4693f89b
toshanighosh019 <toshanighosh019@gmail.com>
2024-12-04 16:26:22 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from "./components/header/header.jsx";
+import Footer from "./components/footer/footer.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./footer.css";
+export default function Footer() {
+    return(
+        <div className ="footer-title footer-font">
+    );
+    <h6>All Right Reserved under 2024 Undertakings</h6>
+        </div>
+    );
+}

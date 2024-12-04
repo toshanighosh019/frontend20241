33b2a51b6ab8de31936ee3164ecdb75e3bb4a64a
toshanighosh019 <toshanighosh019@gmail.com>
2024-12-04 15:11:23 +0530

    frontend

---
+++ src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./header.css";
+export default function Header() {
+    return(
+        <div className ="header-title header-font">
+            <h1>My React App</h1>
+        </div>
+    );
+}

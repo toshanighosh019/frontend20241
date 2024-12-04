c306bb61e807bb04f46b69984dab9f8d69958a87
toshanighosh019 <toshanighosh019@gmail.com>
2024-12-04 15:48:37 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header/header.jsx";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header/>
     </div>
   );
 }
